70a469a9ab413ab02f60deb06d07df639380c765
slhmo <mohamadzadehsaleh7@gmail.com>
2024-11-20 21:55:53 +0330

    made a moving curser

--- main.py
+++ main.py
@@ -21,13 +21,60 @@ def make_game_plain(rows, columns):
         for j1 in range(columns):
             game_plain[i1].append('.')
     i1, j1 = random.randrange(0, rows), random.randrange(0, columns)
-    game_plain[i1][j1] = 'x'
-
+    game_plain[i1][j1] = 'X'
+    return [i1, j1]
 
 dimensions = get_dimensions()
-make_game_plain(rows = dimensions[0], columns = dimensions[1])
+head_position = (
+    make_game_plain(rows = dimensions[0], columns = dimensions[1])[0:2]
+)
 
 for i2 in game_plain:
     print(i2)
+print(head_position)
+
+def move_maker():
+    move = input("choose('w', 's', 'd', 'a'): ")
+
+    if move == 'w':
+        i = 0
+        tmp = game_plain[0][head_position[1]]
+        while i<len(game_plain)-1:
+            game_plain[i][head_position[1]] = game_plain[i+1][head_position[1]]
+            i += 1
+        game_plain[len(game_plain)-1][head_position[1]] = tmp
+
+    if move == 's':
+        i = len(game_plain)-1
+        tmp = game_plain[len(game_plain)-1][head_position[1]]
+        while i>0:
+            game_plain[i][head_position[1]] = game_plain[i-1][head_position[1]]
+            i -= 1
+        game_plain[0][head_position[1]] = tmp
+
+    if move == 'a':
+        if head_position[1] == 0:
+            print('you lost!')
+        else:
+            j = 0
+            tmp = game_plain[head_position[0]][0]
+            while j < len(game_plain[0])-1:
+                game_plain[head_position[0]][j] = game_plain[head_position[0]][j+1]
+                j += 1
+            game_plain[head_position[0]][len(game_plain[0]) - 1] = tmp
+
+    if move == 'd':
+        if head_position[1] == 0:
+            print('you lost!')
+        else:
+            j = len(game_plain[0])-1
+            tmp = game_plain[head_position[0]][len(game_plain[0])-1]
+            while j > 0:
+                game_plain[head_position[0]][j] = game_plain[head_position[0]][j-1]
+                j -= 1
+            game_plain[head_position[0]][0] = tmp
 
 
+move_maker()
+for i2 in game_plain:
+    print(i2)
